25009d044dd8ea508080c37783ec58bf6bea94e2
Andy Tiffany <andy.tiffany@twintechs.com>
2016-05-17 12:49:51 -0700

    updates loader.io verification

--- web.rb
+++ web.rb
@@ -5,6 +5,6 @@ get '/hi' do
 end
 
 # verification token
-get '/loaderio-ec918e838e691ac73089527347514d82/' do
-  "loaderio-ec918e838e691ac73089527347514d82"
+get '/loaderio-b4b9b83c5b5c1cdb8584efa9592820f3/' do
+  "loaderio-b4b9b83c5b5c1cdb8584efa9592820f3"
 end
